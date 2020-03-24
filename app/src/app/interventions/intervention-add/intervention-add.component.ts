cd35b20f9c758108961c7a888b9fa88036b40a1a
Yasmine <yasminehad@hotmail.com>
2020-03-24 18:06:05 +0100

    ajout des equipes au vehicule

--- app/src/app/interventions/intervention-add/intervention-add.component.ts
+++ app/src/app/interventions/intervention-add/intervention-add.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { BrigadeApiService } from "../../services/brigade-api.service";
 import { TypeIntervention } from "../../models/typeIntervention";
 import { Vehicule } from "../../models/vehicule";
+import { RoleVhicule } from "../../models/rolevehicule";
 
 @Component({
   selector: 'app-intervention-add',
   templateUrl: './intervention-add.component.html',
   styleUrls: ['./intervention-add.component.scss']
 })
+
+
 export class InterventionAddComponent implements OnInit {
   response: any;
   typesIntervention: TypeIntervention[];
   vehicules :Vehicule[];
   selectedvehicule: string='';
+  usedVehicule: RoleVhicule[];
+
 
 
   constructor(private apiService: BrigadeApiService) {}
@@ -39,5 +44,20 @@ export class InterventionAddComponent implements OnInit {
       this.vehicules = this.response.vehicules;
       // console.log(this.vehicules);
     });
+  }
+  addTeam(value: string){
+  
+    var val=+value;
+    console.log(value);
+    for (let i of this.vehicules) {
+      if(i.V_ID==val){
+        this.usedVehicule=i.ROLE;
+     
+      }
+   
+  }
+ 
+  
+ 
   }
 }
