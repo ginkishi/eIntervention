32d61e418f17303b54eedf40e64d89e083d84527
Yasmine <yasminehad@hotmail.com>
2020-03-24 14:14:08 +0100

    Update app.module.ts
    ajout du formulaire angular

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -35,7 +36,8 @@ import { InterventionExportComponent } from "./interventions/intervention-export
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
 
   providers: [],
