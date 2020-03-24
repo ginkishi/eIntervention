22a5202fdf26e2e0a17979c11610ed561174dd7d
Yasmine <yasminehad@hotmail.com>
2020-03-24 15:13:23 +0100

    ajout des types des vehicules au formulaire d'ajout d'intervention

--- app/src/app/interventions/intervention-add/intervention-add.component.ts
+++ app/src/app/interventions/intervention-add/intervention-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BrigadeApiService } from "../../services/brigade-api.service";
 import { TypeIntervention } from "../../models/typeIntervention";
+import { Vehicule } from "../../models/vehicule";
 
 @Component({
   selector: 'app-intervention-add',
@@ -10,19 +11,30 @@ import { TypeIntervention } from "../../models/typeIntervention";
 export class InterventionAddComponent implements OnInit {
   response: any;
   typesIntervention: TypeIntervention[];
+  vehicules :Vehicule[];
   constructor(private apiService: BrigadeApiService) {}
 
   ngOnInit(): void {
     this.createListTypeIntervention();
+    this.createListVehicule();
   }
 
-  createListTypeIntervention() {
+  createListTypeIntervention() :void
+  {
       this.apiService.readAllTypeIntervention().subscribe((resultat: TypeIntervention[]) => {
    //   console.log(resultat);
       this.response = JSON.parse(JSON.stringify(resultat));
   //    console.log(this.response);
       this.typesIntervention = this.response.typeIntervention;
     //console.log(this.typesIntervention[0]);
+    });
+  }
+  createListVehicule() :void
+  {
+    this.apiService.readAllVehicule().subscribe((res: Vehicule[]) => {
+      this.response = JSON.parse(JSON.stringify(res));
+      this.vehicules = this.response.vehicules;
+      // console.log(this.vehicules);
     });
   }
 }
