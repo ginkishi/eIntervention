953eb7e0529010f294ef86abb8186b4cae735499
Ginkishi <vicmarvu98@gmail.com>
2020-03-25 17:06:18 +0100

    Ajout du component pour la liste des interventions

---
+++ app/src/app/interventions/intervention-list/intervention-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intervention-list',
+  templateUrl: './intervention-list.component.html',
+  styleUrls: ['./intervention-list.component.scss']
+})
+export class InterventionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
