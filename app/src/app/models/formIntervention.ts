28ca4d3af90f80da1ed5ab31f7dfd044436011be
Yasmine <yasminehad@hotmail.com>
2020-03-26 00:41:32 +0100

    Create formIntervention.ts
    ajouter de l'interface pour recuperer des données  du formualire

---
+++ app/src/app/models/formIntervention.ts
@@ -0,0 +1,18 @@
+export interface FormIntervention 
+{
+    numeroIntervention: number;
+    commune: string;
+    adresse: string;
+    typeIntervention: string;
+    requerant: string;
+    opm: string;
+    important: string;
+    dateDeclenchement: Date;
+    heureDeclenchement: Date;
+    dateFin: Date;
+    heureFin: Date;
+    responsable: string;
+
+
+
+}
