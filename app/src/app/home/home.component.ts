17c4cdd24a0cf0d43c79fa1d7c85feaa86a6e44b
Ginkishi <vicmarvu98@gmail.com>
2020-03-26 12:07:14 +0100

    Recupération de toute les interventions

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { BrigadeApiService } from "../services/brigade-api.service";
 import { Vehicule } from "../models/vehicule";
 import { Pompier } from "../models/pompier";
+import { Intervention } from "../models/intervention";
 
 @Component({
   selector: "app-home",
@@ -12,9 +13,21 @@ export class HomeComponent implements OnInit {
   response: any;
   vehicules: Vehicule[];
   unPompier: Pompier;
+  intervention: Intervention[];
+
   constructor(private apiService: BrigadeApiService) {}
 
   ngOnInit(): void {
-    console.log(localStorage.getItem("setupTime"));
+    // console.log(localStorage.getItem("setupTime"));
+  }
+  getInterventions() {
+    this.apiService
+      .readAllIntervention()
+      .subscribe((resultat: Intervention[]) => {
+        this.response = JSON.parse(JSON.stringify(resultat));
+        //console.log(this.response);
+        this.intervention = this.response.interventions;
+        //console.log(this.intervention);
+      });
   }
 }
