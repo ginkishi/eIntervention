1405344994af4594c0a327bb111e0b0b4ea642d1
Ginkishi <vicmarvu98@gmail.com>
2020-03-27 15:52:59 +0100

    Récupération des données des véhicules

--- app/src/app/vehicules/vehicule-list/vehicule-list.component.ts
+++ app/src/app/vehicules/vehicule-list/vehicule-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Vehicule } from "src/app/models/vehicule";
+import { BrigadeApiService } from "src/app/services/brigade-api.service";
 
 @Component({
-  selector: 'app-vehicule-list',
-  templateUrl: './vehicule-list.component.html',
-  styleUrls: ['./vehicule-list.component.scss']
+  selector: "app-vehicule-list",
+  templateUrl: "./vehicule-list.component.html",
+  styleUrls: ["./vehicule-list.component.scss"]
 })
 export class VehiculeListComponent implements OnInit {
+  response: any;
+  vehicules: Vehicule[];
+  ProfileLoaded: Promise<boolean>;
 
-  constructor() { }
+  constructor(private api: BrigadeApiService) {}
 
   ngOnInit(): void {
+    this.getInformation();
   }
-
+  getInformation() {
+    this.api.readAllVehicule().subscribe((resultat: Vehicule[]) => {
+      this.response = JSON.parse(JSON.stringify(resultat));
+      //console.log(this.response);
+      this.vehicules = this.response.vehicules;
+      this.ProfileLoaded = Promise.resolve(true);
+      //console.log(this.vehicules);
+    });
+  }
 }
