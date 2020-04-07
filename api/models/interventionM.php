134b3ed1786109249c795bb73d72e2d85b4ec7f7
Ginkishi <vicmarvu98@gmail.com>
2020-04-07 10:53:39 +0200

    Création d'une fonction permettant de récupérer toute les interventions non validées par le responsable

--- api/models/interventionM.php
+++ api/models/interventionM.php
@@ -81,6 +81,16 @@ class Intervention
         return $stmt;
     }
 
+    // Récupère la liste des interventions non validé par le responsable
+    public function listAllIntervNoValid()
+    {
+        $sql = 'SELECT IDIntervention,NIntervention,OPM,Commune,Adresse,TypeIntervention,DateDeclenchement,DateFin,Important,IDResponsable,Requerant, s.IDStatus, s.label FROM interventions i JOIN status s on i.IDstatus = s.IDstatus where i.IDstatus = 2';
+        $dbh = BDD::getInstanceOfEIntervention();
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        return $stmt;
+    }
+
     public function listAllIntervUser($id)
     {
         $sql = 'SELECT IDIntervention,NIntervention,OPM,Commune,Adresse,TypeIntervention,DateDeclenchement,DateFin,Important,IDResponsable,Requerant, s.IDStatus, s.label FROM interventions i JOIN status s on i.IDstatus = s.IDstatus where IDResponsable = ' . $id . ";";
