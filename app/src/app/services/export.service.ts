fec5f5b4049019f39a6e107f933b20c12c5255a4
Ginkishi <vicmarvu98@gmail.com>
2020-04-07 15:28:03 +0200

    Ajout de la fonction pour exporter en CSV

--- app/src/app/services/export.service.ts
+++ app/src/app/services/export.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularCsv } from 'angular7-csv/dist/Angular-csv';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExportService {
-
+  data: any;
+  // csvOptions = {
+  //   fieldSeparator: ',',
+  //   quoteStrings: ''',
+  //   decimalseparator: '.',
+  //   showLabels: true,
+  //   showTitle: true,
+  //   title: 'Your Holiday List :',
+  //   useBom: true,
+  //   noDownload: false,
+  //   headers: ['Holiday ID', 'Holiday Date', 'Holiday Comment', 'Holiday Status']
+  // };
   constructor() { }
+
+  exportToCSV(data, nameFile: string, options = null) {
+    new AngularCsv(data, nameFile, options);
+  }
 }
