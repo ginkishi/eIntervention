5dedc2091cc606e268bf0b3711c24a2824cee0e1
Ginkishi <vicmarvu98@gmail.com>
2020-04-07 12:01:18 +0200

    Modification d'intervention-list.component.ts pour récupérer le bon statut d'intervention

--- app/src/app/interventions/intervention-list/intervention-list.component.ts
+++ app/src/app/interventions/intervention-list/intervention-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { BrigadeApiService } from "src/app/services/brigade-api.service";
 import { Intervention } from "src/app/models/intervention";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: "app-intervention-list",
@@ -10,19 +11,57 @@ import { Intervention } from "src/app/models/intervention";
 export class InterventionListComponent implements OnInit {
   intervention: Intervention[];
   response: any;
+  type: string;
 
-  constructor(public api: BrigadeApiService) {}
+  constructor(public api: BrigadeApiService, public routeActive: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.routeActive.data.subscribe(resp => {
+      this.type = Object.values(resp)[0];
+    });
+
     this.getInterventions();
   }
 
   getInterventions() {
-    this.api.readAllIntervention().subscribe((resultat: Intervention[]) => {
-      this.response = JSON.parse(JSON.stringify(resultat));
-      //console.log(this.response);
-      this.intervention = this.response.interventions;
-      //console.log(this.intervention);
-    });
+    switch (this.type) {
+      case 'valid':
+        this.api
+          .readAllInterventionValid()
+          .subscribe((resultat: Intervention[]) => {
+            this.response = JSON.parse(JSON.stringify(resultat));
+            // console.log(this.response);
+            this.intervention = this.response.interventions;
+            console.log(this.intervention);
+          });
+        break;
+      case 'waiting':
+        this.api
+          .readAllInterventionWaiting()
+          .subscribe((resultat: Intervention[]) => {
+            this.response = JSON.parse(JSON.stringify(resultat));
+            // console.log(this.response);
+            this.intervention = this.response.interventions;
+            // console.log(this.intervention);
+          });
+        break;
+      case 'novalid':
+        this.api
+          .readAllInterventionNoValid()
+          .subscribe((resultat: Intervention[]) => {
+            this.response = JSON.parse(JSON.stringify(resultat));
+            // console.log(this.response);
+            this.intervention = this.response.interventions;
+          });
+        break;
+      default:
+        this.api.readAllIntervention().subscribe((resultat: Intervention[]) => {
+          this.response = JSON.parse(JSON.stringify(resultat));
+          // console.log(this.response);
+          this.intervention = this.response.interventions;
+          // console.log(this.intervention);
+        });
+        break;
+    }
   }
 }
