9f754c72a6ed42d94e4b38f190563c99febff971
Yasmine <yasminehad@hotmail.com>
2020-04-08 12:31:01 +0200

    Update intervention-search.component.ts

--- app/src/app/interventions/intervention-search/intervention-search.component.ts
+++ app/src/app/interventions/intervention-search/intervention-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, FormArray } from "@angular/forms";
+import { formatDate } from "@angular/common";
 @Component({
   selector: 'app-intervention-search',
   templateUrl: './intervention-search.component.html',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterventionSearchComponent implements OnInit {
 
+  SearchForm: FormGroup = new FormGroup({
+ 
+  });
   constructor() { }
 
   ngOnInit(): void {
+    this.SearchForm.patchValue({
+      date:formatDate(
+        new Date(),
+        "yyyy-MM-dd",
+        "fr-FR"
+      ),
+      numeroIntervention:null,
+      adresse:null,
+      redacteur:null
+    });
+  }
+
+  onSubmit(){
+    console.log(this.SearchForm.value);
   }
 
 }
