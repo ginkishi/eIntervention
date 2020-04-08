3569f1c6f5df6b5e16c10842ecbf7837c8393409
Yasmine <yasminehad@hotmail.com>
2020-04-08 14:23:58 +0200

    Update intervention-search.component.ts

--- app/src/app/interventions/intervention-search/intervention-search.component.ts
+++ app/src/app/interventions/intervention-search/intervention-search.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormArray } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, NgModel } from "@angular/forms";
 import { formatDate } from "@angular/common";
 import { Pompier } from 'src/app/models/pompier';
 import { BrigadeApiService } from 'src/app/services/brigade-api.service';
 import { DataService } from 'src/app/services/data.service';
+import { Item } from 'src/app/models/item';
+import { Intervention } from 'src/app/models/intervention';
 @Component({
   selector: 'app-intervention-search',
   templateUrl: './intervention-search.component.html',
@@ -11,30 +13,42 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class InterventionSearchComponent implements OnInit {
   
-  SearchForm: FormGroup = new FormGroup({
-
-  });
-  date:string;
+  itemsList: Item[] = [
+    {
+        name:'Numero d\'intervention',
+        value:'numerointervention'
+     },
+     {
+         name:'Date',
+         value:'date'
+      },
+      {
+          name:'Adresse',
+          value:'adresse'
+       },
+       {
+           name:'Redacteur',
+           value:'redacteur'
+        }
+];
+value:string;
+radioSel:any;
+radioSelected:string;
+radioSelectedString:string;
+item:string;
+  numerointervention:string;
   adresse:string;
+  date:string;
   redacteur:string;
-  numeroIntervention:string;
   listePompier: string[] = [];
+  intervention:Intervention[]=[];
   response:any;
   constructor(private apiService: BrigadeApiService,
     private dataService: DataService,
     private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.SearchForm=this.fb.group({
-      date:formatDate(
-        new Date(),
-        "yyyy-MM-dd",
-        "fr-FR"
-      ),
-      numeroIntervention:null,
-      adresse:null,
-      redacteur:null
-    });
+   
     this.createListAllPompier();
   }
   createListAllPompier(): void {
@@ -51,9 +65,44 @@ export class InterventionSearchComponent implements OnInit {
       //console.log(this.typesIntervention[0]);
     });
   }
+  getSelecteditem(){
+    this.radioSel = this.itemsList.find(Item => Item.value === this.radioSelected);
+    this.radioSelectedString = JSON.stringify(this.radioSel);
+  }
 
-  onSubmit(){
-    console.log(this.SearchForm.value);
+  onItemChange(item){
+    this.getSelecteditem();
+  }
+
+  onSubmit(f){
+    this.intervention=[];
+    console.log(f.value);
+    console.log(this.radioSelected);
+    console.log(f.value.adresse);
+    if(f.value.item='adresse')
+    {
+
+    }
+   if(f.value.item='numerointervention')
+    {
+      this.dataService.getInterventionsbyNum(f.value.numerointervention).subscribe((resultat: Intervention[]) => {
+        this.response = JSON.parse(JSON.stringify(resultat));
+        this.intervention = this.response.interventions;
+     
+        console.log(this.intervention);
+      });
+     
+    }
+    if(f.value.item='date')
+    {
+      
+    }
+    if(f.value.item='redacteur')
+    {
+      
+    }
+   
+   
   }
 
 }
