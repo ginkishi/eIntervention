3fb095b7d640526c05ee260f240b980c569fbe40
Yasmine <yasminehad@hotmail.com>
2020-04-09 10:24:51 +0200

    Update interventionM.php

--- api/models/interventionM.php
+++ api/models/interventionM.php
@@ -115,7 +115,7 @@ class Intervention
     public function getInterventionByNum($id)
     {
         $id = self::cleanUserInput($id);
-        $sql = "SELECT i.IDIntervention,NIntervention,OPM,Commune,Adresse,TypeIntervention,DateDeclenchement,DateFin,Important,IDResponsable,Requerant,i.IDStatus,s.label FROM interventions i JOIN status s on i.IDstatus = s.IDstatus where NIntervention = " . $id . ";";
+        $sql = "SELECT i.IDIntervention,i.NIntervention,i.OPM,i.Commune,i.Adresse,i.TypeIntervention,i.DateDeclenchement,i.DateFin,i.Important,i.IDResponsable,i.Requerant,i.IDStatus,s.label FROM interventions as i JOIN status as s on i.IDstatus = s.IDstatus where i.NIntervention = " . $id . ";";
         $dbh = BDD::getInstanceOfEIntervention();
         $stmt = $dbh->prepare($sql);
         $stmt->execute();
