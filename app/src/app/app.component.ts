0785ac30868772acb3c3a0d98e6667f2dbea603b
Ginkishi <vicmarvu98@gmail.com>
2020-04-25 12:25:14 +0200

    Linting de l'app.component

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { Component, ViewEncapsulation } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = "eIntervention";
+  title = 'eIntervention';
   faCoffee = faCoffee;
 }
